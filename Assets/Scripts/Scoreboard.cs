e6dc351277e94e6ac57b7bc1f7becae1cdad03d0
Cristian Pîrvu <5724259+Kreestijan@users.noreply.github.com>
2022-07-10 15:27:38 +0300

    Added scoreboard base

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using System.Linq;
+
+public class Scoreboard : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Transform container;
+    [SerializeField] GameObject scoreboardItemPrefab;
+
+    Dictionary<Player, ScoreboardItem> scoreboardItems = new();
+
+    private void Start()
+    {
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            AddScoreboardItem(player);
+        }
+    }
+
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        AddScoreboardItem(newPlayer);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        RemoveScoreboardItem(otherPlayer);
+    }
+
+    void AddScoreboardItem(Player player)
+    {
+        ScoreboardItem item = Instantiate(scoreboardItemPrefab, container).GetComponent<ScoreboardItem>();
+        item.Initialize(player);
+        scoreboardItems[player] = item;
+    }
+
+    void RemoveScoreboardItem(Player player)
+    {
+        Destroy(scoreboardItems[player].gameObject);
+        scoreboardItems.Remove(player);
+    }
+
+
+}
