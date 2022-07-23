ae6a08c400967e467c503143f201ea8b487135fd
Sebastianski <sebirobz@gmail.com>
2022-07-23 16:43:59 +0300

    wave-system

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,12 +7,13 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     GameObject enemy;
-    public MyEnum myDropDown = new MyEnum();
-
-
-   
+    public MyEnum EnemySelection = new MyEnum();
+    [SerializeField] Transform parent;
     PhotonView PV;
-
+    int counter=5;
+    bool cond;
+    private IEnumerator coroutineSpawn, coroutineStart;
+    bool CR;
     private void Awake()
     {
         PV = GetComponent<PhotonView>();
@@ -20,40 +21,67 @@ public class EnemySpawner : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(EnemySpawnDelay());
+        coroutineSpawn = EnemySpawnDelay();
+        coroutineStart = StartTheCoroutine();
+        StartCoroutine(coroutineSpawn);
         
     }
+
     void Update()
     {
-        if(myDropDown.ToString()== "EnemyZombie")
-        {
-            Debug.Log("EnemyZombie");
+        if (GameObject.Find("EnemySpawner").transform.childCount >= counter)
+        { StopCoroutine(coroutineSpawn);
+            Debug.Log("S-a oprit corutina de Spawn");
+            cond = true;
         }
-        else
-            Debug.Log("Rhino");
+        Debug.Log("Numarul de inamici este :" + GameObject.Find("EnemySpawner").transform.childCount);
+        if (GameObject.Find("EnemySpawner").transform.childCount == 0)
+        {
+            StartCoroutine(coroutineStart);
+            Debug.Log("A inceput corutina de Start");
+            CR = true;
+            counter += 5;
+        }
 
-        /*var dropDown1 = MyEnum.EnemyZombie;
-        var dropDown2 = MyEnum.Rhino_PBR;*/
+        if (CR == true)
+        { 
+                StartCoroutine(coroutineSpawn);
+                Debug.Log("A inceput corutina de Spawn");
+                CR = false;
+        }
+
     }
-
     private void CreateEnemy()
     {
-        
         Vector3 spawnpoint = new(transform.position.x + Random.Range(-3, 3), transform.position.y, transform.position.z + Random.Range(-3, 3));
-        //var dropDown1 = MyEnum.EnemyZombie;
-        enemy = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", myDropDown.ToString()), spawnpoint, new Quaternion(transform.rotation.x, Random.rotation.y, transform.rotation.z, Quaternion.identity.w), 0, new object[] { PV.ViewID });
+        enemy = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", EnemySelection.ToString()), spawnpoint, new Quaternion(transform.rotation.x, Random.rotation.y, transform.rotation.z, Quaternion.identity.w), 0, new object[] { PV.ViewID });
+        enemy.transform.parent=gameObject.transform;
     }
+
     public enum MyEnum
     {
         EnemyZombie,
         Rhino_PBR,
     };
+
     private IEnumerator EnemySpawnDelay()
     {
-        while (true)
+        cond = true;
+        while (cond==true)
         {
             yield return new WaitForSeconds(2.0f);
             CreateEnemy();
+            
         }
+        
+
     }
+    private IEnumerator StartTheCoroutine()
+    {
+            yield return new WaitForSeconds(5.0f);
+            
+
+    }
+
+
 }
