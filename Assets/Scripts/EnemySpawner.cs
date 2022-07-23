2f2d7b573f81b71205b3e62d95dd1125cd648e31
Sebastianski <sebirobz@gmail.com>
2022-07-23 13:09:31 +0300

    enemy-spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,7 +7,10 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     GameObject enemy;
+    public MyEnum myDropDown = new MyEnum();
 
+
+   
     PhotonView PV;
 
     private void Awake()
@@ -18,18 +21,33 @@ public class EnemySpawner : MonoBehaviour
     private void Start()
     {
         StartCoroutine(EnemySpawnDelay());
+        
     }
     void Update()
     {
-        
+        if(myDropDown.ToString()== "EnemyZombie")
+        {
+            Debug.Log("EnemyZombie");
+        }
+        else
+            Debug.Log("Rhino");
+
+        /*var dropDown1 = MyEnum.EnemyZombie;
+        var dropDown2 = MyEnum.Rhino_PBR;*/
     }
 
     private void CreateEnemy()
     {
+        
         Vector3 spawnpoint = new(transform.position.x + Random.Range(-3, 3), transform.position.y, transform.position.z + Random.Range(-3, 3));
-        enemy = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "EnemyZombie"), spawnpoint, new Quaternion(transform.rotation.x, Random.rotation.y, transform.rotation.z, Quaternion.identity.w), 0, new object[] { PV.ViewID });
+        //var dropDown1 = MyEnum.EnemyZombie;
+        enemy = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", myDropDown.ToString()), spawnpoint, new Quaternion(transform.rotation.x, Random.rotation.y, transform.rotation.z, Quaternion.identity.w), 0, new object[] { PV.ViewID });
     }
-
+    public enum MyEnum
+    {
+        EnemyZombie,
+        Rhino_PBR,
+    };
     private IEnumerator EnemySpawnDelay()
     {
         while (true)
